1513db2839887228a3b4237126017cdcda1f1109
karripar <karripartanen25@gmail.com>
2025-03-05 15:34:01 +0200

    next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,43 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+           <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">
+          Example Site
+        </span>
+      </div>
+      <div className="lg:flex lg:items-center lg:w-auto justify-end">
+        <ul className="flex">
+          <li className="mr-4">
+
+            <Link
+              href="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="mr-4">
+            <a
+              href="/profile"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Profile
+            </a>
+          </li>
+          <li className="mr-4">
+            <a
+              href="/upload"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Upload
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div className="container mx-auto pt-4">{children}</div>
       </body>
     </html>
   );
---
+++ src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">
+        Upload
+      </h1>
+    </main>
+  );
+};
+
+export default Page;
