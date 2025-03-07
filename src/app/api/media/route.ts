e4cb1216528c710617c679373565b1b4caa3a8de
karripar <karripartanen25@gmail.com>
2025-03-07 11:37:47 +0200

    update

---
+++ src/app/api/media/route.ts
@@ -0,0 +1,41 @@
+import { fetchData } from '@/lib/functions';
+import { postMedia } from '@/models/mediaModel';
+import { MediaItem, UploadResponse} from 'hybrid-types'; 
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  try {
+    // TODO: get the form data from the request
+    const formData = await request.formData();
+    // TODO: get the token from the cookie
+    const session = request.cookies.get('session')?.value as string;
+    
+    if (!session) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+    // TODO: send the form data to the uppload server. See apiHooks from previous classes.
+    // TODO: if the upload response is not valid, return an error response with NextResponse
+    // TODO: get title and description from the form data
+    // TODO: get the filename, filesize and media_type  from the upload response
+    // TODO: get user_id from getSession() function
+    // TODO: create a media item object, see what postMedia funcion in mediaModel wants for input.
+    // TODO: use the postMedia function from the mediaModel to add the media to the database. Since we are putting data to the database in the same app, we dont need to use a token.
+
+    if (!postResult) {
+      return new NextResponse('Error adding media to database', { status: 500 });
+    }
+
+    const uploadResponse: UploadResponse = {
+      message: 'Media added to database',
+      data: postResult,
+    };
+
+    return new NextResponse(JSON.stringify(uploadResponse), {
+      headers: { 'content-type': 'application/json' },
+    });
+  } catch (error) {
+    console.error((error as Error).message, error);
+    return new NextResponse((error as Error).message, { status: 500 });
+  }
+}
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,14 +1,16 @@
+import MediaForm from "@/components/MediaForm";
 import { requireAuth } from "@/lib/authActions";
 
-const Page = async () => {
+const Upload = async () => {
   await requireAuth();
   return (
     <main>
       <h1 className="text-4xl font-bold">
         Upload
       </h1>
+      <MediaForm />
     </main>
   );
 };
 
-export default Page;
+export default Upload;
